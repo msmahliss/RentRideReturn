6fcaa1d492de08167448a548a465c0363e225191
Melissa <msmahliss@gmail.com>
2015-08-29 23:12:07 -0400

    close bookings 8/30/15

--- static/js/main.js
+++ static/js/main.js
@@ -109,7 +109,7 @@ function displayValidDates(date) {
  }
 
 function closeBookings(date){
-	if (date=="08/29/2015"){
+	if (date=="08/30/2015"){
 		$('.close-message').show();
 		$('.js-rental-qty option').each(function(){
 			 $(this).attr('disabled', true);
